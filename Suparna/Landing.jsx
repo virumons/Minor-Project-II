fa06dfdbc7c9d5c292f3c5e31da807d32849c7d8
Suparna-Kulkarni <138349397+Suparna-Kulkarni@users.noreply.github.com>
2024-03-01 20:54:41 +0530

    Update Landing.jsx

---
+++ Suparna/Landing.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css'
+function Landing(){
+  return(
+    <>
+     <div className="flex flex-col hero_section h-[100vh]">
+      
+      <div className="flex flex-row justify-between item-center px-10 py-10">
+      <label className='flex flex-row item-center justify-center'>
+      <img src="whitelogo.svg" alt="Logo" className="h-auto"></img>
+        <h1 className="font-semibold text-xl mt-2 ml-2">Fileit</h1>
+        </label>
+        <button className="bg-blue-500 text-white font-semibold px-16 py-3 rounded-lg">Login</button>
+      </div>
+      
+      
+      <div className="px-12">
+      <p className="font-semibold text-blue-500 text-8xl mt-20">Securely</p>
+      <p className="font-semibold text-7xl mr-11">Send files</p>
+      <p className="mt-2 font-semibold">Fileit Business is a secure file sharing and storage solution <br/> that employees love and IT admins trust.</p>
+      <button className="bg-blue-500 text-white font-semibold px-16 py-3 rounded-lg mt-5">Get started</button>
+      </div>
+    
+    </div>
+    </>
+  )
+}
+
+export default Landing
